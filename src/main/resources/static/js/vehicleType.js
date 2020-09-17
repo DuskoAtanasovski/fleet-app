7e58302fc7033b891906e957afb57c8a0fb14e4c
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-09-17 03:44:28 +0200

    Add existing file

--- src/main/resources/static/js/vehicleType.js
+++ src/main/resources/static/js/vehicleType.js
@@ -29,7 +29,7 @@ $('document').ready(function() {
 
 	});
 
-	$('.table #deleteButton').on('click', function(event) {
+	$('table #deleteButton').on('click', function(event) {
 		event.preventDefault();
 		var href = $(this).attr('href');
 		$('#confirmDeleteButton').attr('href', href);
