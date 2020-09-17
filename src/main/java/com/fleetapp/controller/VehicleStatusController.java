0014f4fea01b2e36390ba81d1639280e03dd7573
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-09-17 02:46:15 +0200

    Add existing file

--- src/main/java/com/fleetapp/controller/VehicleStatusController.java
+++ src/main/java/com/fleetapp/controller/VehicleStatusController.java
@@ -1,14 +1,58 @@
 package com.fleetapp.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fleetapp.model.VehicleStatus;
+import com.fleetapp.service.VehicleStatusService;
 
 @Controller
 public class VehicleStatusController {
-	
+
+	@Autowired
+	private VehicleStatusService vehicleStatusService;
+
 	@GetMapping("/vehicleStatuses")
-	public String getVehicleStatuses() {
+	public String getVehicleStatus(Model model) {
+
+		List<VehicleStatus> vehicleStatusList = vehicleStatusService.getVehicleStatuss();
+		model.addAttribute("vehicleStatuses", vehicleStatusList);
+
 		return "VehicleStatus";
+
+	}
+
+	@PostMapping("/vehicleStatuses/addNew")
+	public String addNew(VehicleStatus vehicleStatus) {
+		vehicleStatusService.save(vehicleStatus);
+		return "redirect:/vehicleStatuses";
+	}
+
+	@RequestMapping("vehicleStatuses/findById")
+	@ResponseBody
+	public Optional<VehicleStatus> findById(int id) {
+		return vehicleStatusService.findById(id);
+	}
+
+	@RequestMapping(value = "/vehicleStatuses/update", method = { RequestMethod.PUT, RequestMethod.GET })
+	public String update(VehicleStatus vehicleStatus) {
+		vehicleStatusService.save(vehicleStatus);
+		return "redirect:/vehicleStatuses";
+	}
+
+	@RequestMapping(value = "/vehicleStatuses/delete", method = { RequestMethod.DELETE, RequestMethod.GET })
+	public String delete(Integer id) {
+		vehicleStatusService.delete(id);
+		return "redirect:/vehicleStatuses";
 	}
 
 }
