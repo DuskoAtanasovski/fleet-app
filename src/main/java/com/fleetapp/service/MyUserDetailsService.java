e2425d506dd78d9027db460b95c7d35493e5397c
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-09-21 20:42:49 +0200

    Add existing file

---
+++ src/main/java/com/fleetapp/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.fleetapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fleetapp.model.User;
+import com.fleetapp.model.UserPrincipal;
+import com.fleetapp.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found!");
+		}
+
+		return new UserPrincipal(user);
+	}
+
+}
